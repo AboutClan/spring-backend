28ffbea910cbd1d5c85851faaceecd723444587d
beming-dev <mingfordev@gmail.com>
2024-06-23 13:18:45 +0900

    Init

---
+++ src/main/java/com/about_backend/studyabout/StudyaboutApplication.java
@@ -0,0 +1,13 @@
+package com.about_backend.studyabout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyaboutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyaboutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/about_backend/studyabout/StudyaboutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.about_backend.studyabout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyaboutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
